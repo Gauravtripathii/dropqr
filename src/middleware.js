cb6403dcfbad9440fdf7e5ef0aad3c3824af7917
gauravtripathii <gauravtripathii7979@gmail.com>
2025-02-16 13:17:30 +0530

    middleware added

---
+++ src/middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath = path === '/' || path === '/auth';
+
+    const token = request.cookies.get('token')?.value || "";
+
+    if (isPublicPath && token)
+        return NextResponse.redirect(new URL('/upload', request.nextUrl));
+
+    if (!isPublicPath && !token)
+        return NextResponse.redirect(new URL('/auth', request.nextUrl));
+
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/auth',
+        // '/profile/:path*',
+        '/upload',
+        // '/verifyemail'
+    ]
+}
