8c024b90dc64727eb4b164d6e108feeb24610701
gauravtripathii <gauravtripathii7979@gmail.com>
2025-04-29 12:22:27 +0530

    payments all commented for now to remove all the potential errors for deployment (vercel)

--- src/app/api/payments/v1/order/route.js
+++ src/app/api/payments/v1/order/route.js
@@ -1,26 +1,26 @@
-import Razorpay from "razorpay";
-import { NextResponse } from "next/server";
+// import Razorpay from "razorpay";
+// import { NextResponse } from "next/server";
 
-const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+// const razorpay = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET,
+// });
 
-export async function POST(request) {
-    try {
-        const { amount, currency } = await request.json();
+// export async function POST(request) {
+//     try {
+//         const { amount, currency } = await request.json();
 
-        var options = {
-            amount,
-            currency,
-        };
+//         var options = {
+//             amount,
+//             currency,
+//         };
 
-        const order = razorpay.orders.create(options);
+//         const order = razorpay.orders.create(options);
 
-        const orderId = (await order).id;
+//         const orderId = (await order).id;
 
-        return NextResponse.json({ orderId, success: true }, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-}
+//         return NextResponse.json({ orderId, success: true }, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ error: error.message }, { status: 500 });
+//     }
+// }
